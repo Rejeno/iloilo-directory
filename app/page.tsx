2c3ff251c982f99a5519534d5575acb911327925
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-04-16 00:50:17 +0800

    feat: add popular categories section of homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 
 const HomePage = () => {
   return (
     <main className="min-h-screen bg-gray-50 font-sans">
       
    {/* Hero Section */}
-      <section className="relative w-full h-125 flex flex-col items-center justify-center text-center px-4 overflow-hidden -mt-25 z-0">
+      <section className="font-sora relative w-full h-125 flex flex-col items-center justify-center text-center px-4 overflow-hidden -mt-25 z-0">
         
         {/* Background Image */}
         <img 
@@ -28,6 +27,35 @@ const HomePage = () => {
         </div>
       </section>
 
+    {/* Main Content Container */}
+      <div className="font-sora max-w-7xl mx-auto px-4 py-12 space-y-16">
+        
+       {/* Popular Categories */}
+        <section>
+          <div className="flex justify-between items-center mt-12 mb-12">
+            <h2 className="text-5xl font-bold text-gray-800">Popular Categories</h2>
+            <button className="bg-blue-600 text-white px-5 py-2 rounded-md font-medium hover:bg-blue-700 transition flex items-center gap-2 cursor-pointer">
+              View All <span>▶</span>
+            </button>
+          </div>
+          <div className="grid grid-cols-4 gap-6">
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+              <div key={item} className="relative rounded-xl overflow-hidden group cursor-pointer h-62 shadow-sm">
+                <img 
+                  src="/home-assets/ilobed.jpg" 
+                  alt="Category" 
+                  className="w-full h-full object-cover object-center group-hover:scale-105 transition-transform duration-300" 
+                />
+                <div className="absolute bottom-0 left-0 right-0 bg-[#115FC8]/80 text-white py-3 px-3 text-center z-10">
+                  <p className="font-bold text-xl leading-tight">Government & Orgs</p>
+                  <p className="text-sm font-medium text-white mt-1">117 Listings</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+
+      </div>
     </main>
   );
 };
