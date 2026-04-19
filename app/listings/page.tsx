eab2561c66ce0b32bdc5581608bba4db12cae797
Wracell <rhazeljay.gumacal@wvsu.edu.ph>
2026-04-19 22:43:52 +0800

    feat: add top controls section of listings

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -38,6 +38,49 @@ export default function ListingsPage() {
         </div>
       </section>
 
+     {/* Main Content Area */}
+      <div className="font-sora max-w-7xl mx-auto px-8 pt-8">
+        
+        {/* Top Controls: Count, Title, and Filters */}
+        <div className="mb-6">
+          <p className="text-sm text-[#343434] mb-5">Showing 1 - 40 of 10,085 Results</p>
+          <div className="flex flex-row items-center justify-between gap-4">
+            <h2 className="text-[40px] font-bold text-[#343434]">Discover New Places</h2>
+            
+            <div className="flex items-center gap-3">
+              <div className="flex items-center bg-[#FAFAEF] border border-gray-200 rounded-xl px-3 py-2 drop-shadow-md">
+                <span className="text-base text-[#827D7D] mr-2">Sort by:</span>
+                <select className="bg-transparent text-base font-medium text-gray-700 outline-none cursor-pointer">
+                  <option>Default</option>
+                  <option>Newest</option>
+                  <option>Popular</option>
+                </select>
+              </div>
+              
+            <div className="flex items-center gap-3">
+                {/* Grid View Button (Active) */}
+                <button className="flex items-center justify-center w-10 h-10 pt-2">
+                    <img 
+                    src="/listings-assets/one.svg" 
+                    alt="Grid View" 
+                    className="w-10 h-10 object-contain" 
+                    />
+                </button>
+                
+                {/* List View Button (Inactive) */}
+                <button className="flex items-center justify-center w-10 h-10 bg-[#FAFAEF] rounded-xl shadow-md">
+                    <img 
+                    src="/listings-assets/two.svg"  
+                    alt="List View" 
+                    className="w-8 h-8 object-contain" 
+                    />
+                </button>
+            </div>
+            </div>
+          </div>
+        </div>
+
+    </div>
     </main>
   );
 }
